b0aabd7803f814ac9010ca8e01d8dc0a2d3aadf3
Stefan Piazza <stefanpiazza@gmail.com>
2017-12-29 15:35:39 +0000

    REFACTOR: change to stateless component using fat arrow notation

--- src/components/Pun/Pun.js
+++ src/components/Pun/Pun.js
@@ -4,19 +4,31 @@ var styles = require('./Pun.scss');
 
 import React from 'react';
 
-class Pun extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+// class Pun extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return (
+//             <div className='pun'>
+//                 <p className='joke'>{ this.props.joke }</p>
+//                 <p className='punch-line'>{ this.props.punchLine }</p>
+//             </div>
+//         )
+//     }
+// }
 
-    render() {
-        return (
-            <div className='pun'>
-                <p className='joke'>{ this.props.joke }</p>
-                <p className='punch-line'>{ this.props.punchLine }</p>
-            </div>
-        )
-    }
+const Pun = ({ joke, punchLine }) => {
+    return (
+        <div className='pun'>
+            <Joke text={ joke } />
+            <PunchLine text={ punchLine } />
+        </div>
+    )
 }
+
+const Joke = ({ text }) => <p className='joke'>{ text }</p>
+const PunchLine = ({ text }) => <p className='punch-line'>{ text }</p>
 
 export default Pun
