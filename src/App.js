23c0a8ed2191bc31a7f953443b34e7bfda70dfb4
Stefan Piazza <stefanpiazza@gmail.com>
2017-12-29 15:12:05 +0000

    UPDATE: Remove loading message

--- src/App.js
+++ src/App.js
@@ -11,30 +11,41 @@ import * as firebase from 'firebase';
 import Button from './components/Button/Button';
 import Pun from './components/Pun/Pun';
 
-const data = require('./data.json');
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: [],
             currentIndex: 0,
-            pun: data[0]
+            pun: {
+                joke: "Preparing jokes",
+                punchLine: "Get ready to groan."
+            }
         }
 
         this.update = this.update.bind(this);
     }
 
     componentWillMount() {
-
+        const db = firebase.database();
+        const dbRef = db.ref();
+        const punsRef = dbRef.child('puns');
+        punsRef.once('value', snap => {
+            this.setState({
+                data: snap.val(),
+                currentIndex: 0,
+                pun: snap.val()[0]
+            })
+        });
     }
 
     update() {
-        let filteredData = data.filter(item => data.indexOf(item) !== this.state.currentIndex);
+        let filteredData = this.state.data.filter(item => this.state.data.indexOf(item) !== this.state.currentIndex);
         let dataIndex = Math.floor(Math.random() * filteredData.length);
         let item = filteredData[dataIndex];
 
         this.setState({
-            currentIndex: data.indexOf(item),
+            currentIndex: this.state.data.indexOf(item),
             pun: item
         })
     }
@@ -44,7 +55,7 @@ class App extends React.Component {
             <div className='app'>
                 <div className='container'>
                     <Pun joke={ this.state.pun.joke } punchLine={ this.state.pun.punchLine } />
-                    <Button text='Another Pun' onClick={ this.update } />
+                    <Button text='Tell Me Another' onClick={ this.update } />
                 </div>
             </div>
         )
