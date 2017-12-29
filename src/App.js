2eb1e42a82e8e609cc7a0db92861586b58439ed7
Stefan Piazza <stefanpiazza@gmail.com>
2017-12-29 15:51:36 +0000

    UPDATE: init service worker in app constructor

--- src/App.js
+++ src/App.js
@@ -24,6 +24,16 @@ class App extends React.Component {
         }
 
         this.update = this.update.bind(this);
+
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js').then(registration => {
+                    console.log('SW registered: ', registration);
+                }).catch(registrationError => {
+                    console.log('SW registration failed: ', registrationError);
+                });
+            });
+        }
     }
 
     componentWillMount() {
