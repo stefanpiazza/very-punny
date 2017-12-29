1e34b88eb5798381827d670a6b8821fc181fae8a
Stefan Piazza <stefanpiazza@gmail.com>
2017-12-29 15:52:01 +0000

    UPDATE: workbox config added

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -104,5 +105,12 @@ module.exports = {
             template: "./src/index.html",
             title: "Very Punny"
         }),
+        new WorkboxPlugin({
+            globDirectory: 'dist',
+            globPatterns: ['**/*.{html,js,css}'],
+            swDest: path.join('dist', 'sw.js'),
+            clientsClaim: true,
+            skipWaiting: true,
+          })
     ]
 }
