20d9df015b84d98e96275e3fc8424bad3d9a0b85
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-01 17:17:51 +0000

    UPDATE: punlist view component

---
+++ src/components/PunList/PunList.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var styles = require('./PunList.scss');
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import * as firebase from 'firebase';
+
+import Loading from '../Loading/Loading';
+import Pun from '../Pun/Pun';
+
+class PunList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isLoading: true
+        }
+    }
+
+    componentWillMount() {
+        const db = firebase.database();
+        const dbRef = db.ref();
+        const punsRef = dbRef.child('puns');
+        punsRef.once('value', snap => {
+            this.setState({
+                data: snap.val(),
+                isLoading: false
+            })
+        });
+    }
+
+    render() {
+        let loading = null;
+        let punList = null;
+
+        if (!this.state.isLoading) {
+            punList = this.state.data.map((pun) =>
+                <li className='pun-list__item'>
+                    <Pun joke={ pun.joke } punchLine={ pun.punchLine } />
+                </li>
+            );
+
+            loading = null;
+        }
+
+        else {
+            loading = <Loading text='Loading...' />
+        }
+
+        return (
+            <ul className='pun-list'>
+                { loading }
+                { punList }
+            </ul>
+        );
+    }
+}
+
+export default PunList
