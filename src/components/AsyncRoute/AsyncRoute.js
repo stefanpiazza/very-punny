0cac224faa71206b5aee9a86f63f194ecb29102a
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-14 12:07:58 +0000

    UPDATE: asyncroute class added

---
+++ src/components/AsyncRoute/AsyncRoute.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class AsyncRoute extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        this.props.loading.then(module => {
+            this.component = module.default;
+            this.setState({
+                loading: false
+            })
+        })
+    }
+
+    render() {
+        if (!this.state.loading) {
+            return <this.component {...this.props.props} />
+        }
+    }
+}
+
+export default AsyncRoute
