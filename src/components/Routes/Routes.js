58fe1f0641409e99d9cc92fa60e7254ed72039e8
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-14 21:29:18 +0000

    UPDATE: async home route

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,9 +3,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from '../Home/Home';
-import NotFound from '../NotFound/NotFound';
-
 import AsyncRoute from '../AsyncRoute/AsyncRoute';
 
 class Routes extends React.Component {
@@ -16,9 +13,9 @@ class Routes extends React.Component {
     render() {
         return (
             <Switch>
-                <Route exact path='/' component={ Home } />
-                <Route path='/all' component={ props => <AsyncRoute props={ props } loading={ System.import('../PunList/PunList') } />} />
-                <Route component={ props => <AsyncRoute props={ props } loading={ System.import('../NotFound/NotFound') } />} />
+                <Route exact path='/' component={ (props) => <AsyncRoute props={ props } loading={ System.import('../Home/Home') } />} />
+                <Route path='/all' component={ (props) => <AsyncRoute props={ props } loading={ System.import('../PunList/PunList') } />} />
+                <Route component={ (props) => <AsyncRoute props={ props } loading={ System.import('../NotFound/NotFound') } />} />
             </Switch>
         );
     }
