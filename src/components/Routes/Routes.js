283852169fad5f76c7b5a8d5413143a70a17358f
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-20 17:35:44 +0000

    UPDATE: admin route added

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -15,6 +15,7 @@ class Routes extends React.Component {
             <Switch>
                 <Route exact path='/' component={ (props) => <AsyncRoute props={ props } loading={ System.import('../Home/Home') } />} />
                 <Route path='/all' component={ (props) => <AsyncRoute props={ props } loading={ System.import('../PunList/PunList') } />} />
+                <Route path='/admin' component={ (props) => <AsyncRoute props={ props } loading={ System.import('../Admin/Admin') } />} />
                 <Route component={ (props) => <AsyncRoute props={ props } loading={ System.import('../NotFound/NotFound') } />} />
             </Switch>
         );
