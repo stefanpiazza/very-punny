92ab146b5bb3846d5a57d0c70f6c9893e18ba403
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-24 22:20:08 +0000

    UPDATE: all component added as container for punList

--- src/components/All/All.js
+++ src/components/All/All.js
@@ -1,1 +1,56 @@
-// TODO
+'use strict';
+
+var styles = require('./All.scss');
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import * as firebase from 'firebase';
+
+import Loading from '../Loading/Loading';
+import PunList from '../PunList/PunList';
+
+class All extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isLoading: true
+        }
+    }
+
+    componentWillMount() {
+        const db = firebase.database();
+        const dbRef = db.ref();
+        const punsRef = dbRef.child('puns');
+        punsRef.once('value', snap => {
+            this.setState({
+                data: snap.val(),
+                isLoading: false
+            })
+        });
+    }
+
+    render() {
+        let loading = null;
+        let punList = null;
+
+        if (!this.state.isLoading) {
+            punList = <PunList puns={ this.state.data } />
+            loading = null;
+        }
+
+        else {
+            loading = <Loading text='Loading...' />
+        }
+
+        return (
+            <div className='all'>
+                { loading }
+                { punList }
+            </div>
+        );
+    }
+}
+
+export default All
