d1e378a8142d0c9afc79470dfaa00732ec5bfdae
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-27 14:03:43 +0000

    UPDATE: return <ul> with items inside to remove need for div wrapper

--- src/components/PunList/PunList.js
+++ src/components/PunList/PunList.js
@@ -8,14 +8,11 @@ import { render } from 'react-dom';
 import Pun from '../Pun/Pun';
 
 const PunList = ({ puns }) => {
-    const punList = (puns) => <ul className='pun-list'>{ punListItems(puns) }</ul>
     const punListItems = (puns) => puns.map((pun, index) => punListItem(pun, index));
     const punListItem = (pun, index) =><li className='pun-list__item' key={ index } ><Pun joke={ pun.joke } punchLine={ pun.punchLine } /></li>
 
     return (
-        <div>
-            { punList(puns) }
-        </div>
+        <ul className='pun-list'>{ punListItems(puns) }</ul>
     )
 }
 
