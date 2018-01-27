fcbb97980f5c247641b7498c22a7c4ac3a1fa62b
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-27 14:03:57 +0000

    UPDATE: navigation component added

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var styles = require('./Navigation.scss');
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        }
+
+        this.openNavigation = this.openNavigation.bind(this);
+        this.closeNavigation = this.closeNavigation.bind(this);
+        this.toggleNavigation = this.toggleNavigation.bind(this);
+    }
+
+    openNavigation() {
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    closeNavigation() {
+        this.setState({
+            isOpen: false
+        })
+    }
+
+    toggleNavigation() {
+        if (this.state.isOpen) {
+            this.closeNavigation();
+        }
+
+        else if (!this.state.isOpen) {
+            this.openNavigation();
+        }
+    }
+
+    render() {
+        let navClass = (this.state.isOpen ? 'nav is-open' : 'nav');
+
+        return (
+            <nav className={ navClass }>
+                <ul className='nav__list'>
+                    <li className='nav__list-item'>
+                        <Link to='/' className='nav__link'>Home</Link>
+                    </li>
+                    <li className='nav__list-item'>
+                        <Link to='/all' className='nav__link'>All</Link>
+                    </li>
+                </ul>
+                <div className='nav__toggle' onClick={ this.toggleNavigation }></div>
+            </nav>
+        )
+    }
+}
+
+export default Navigation
