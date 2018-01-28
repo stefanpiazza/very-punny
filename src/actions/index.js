82205f31b9f5fef7e76cc06c42c3d97b6e4d6a7c
Stefan Piazza <stefanpiazza@gmail.com>
2018-01-28 16:08:39 +0000

    UPDATE: actions for adding and removing puns from store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,34 @@
-export const userLogIn = (user) => {
+const userLogIn = (user) => {
     return {
         type: 'USER_LOGGED_IN',
         payload: user
     }
 }
 
-export const userLogOut = (user) => {
+const userLogOut = (user) => {
     return {
         type: 'USER_LOGGED_OUT',
         payload: user
     }
+}
+
+const addPun = (pun) => {
+    return {
+        type: 'ADDED_PUN',
+        payload: pun
+    }
+}
+
+const removePun = (pun) => {
+    return {
+        type: 'REMOVED_PUN',
+        payload: pun
+    }
+}
+
+export {
+    userLogIn,
+    userLogOut,
+    addPun,
+    removePun
 }
