dad8aef7b4298c8e05495ba9e3e3663a4a45221c
Stefan Piazza <stefanpiazza@gmail.com>
2018-02-10 12:05:42 +0000

    UPDATE: fill in user reducer logic

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,15 +1,13 @@
-const initialState = [{
-
-}];
+const initialState = null;
 
 export default (state = initialState, action) => {
     switch(action.type) {
         case 'USER_LOGGED_IN':
-            return action.payload;
+            return action.payload
             break;
 
         case 'USER_LOGGED_OUT':
-            return action.payload;
+            return null
             break;
 
         default:
