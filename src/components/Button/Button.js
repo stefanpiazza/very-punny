312bbffcc175a4bc8716197b1f32b6bed94f61c2
Stefan Piazza <stefanpiazza@gmail.com>
2018-02-10 19:02:30 +0000

    REFACTOR: tidy up code

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,18 +5,6 @@ var styles = require('./Button.scss');
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// class Button extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//
-//     render() {
-//         return (
-//             <div className='button' onClick={ this.props.onClick }>{ this.props.text }</div>
-//         )
-//     }
-// }
-
 const Button = ({ onClick, text }) => {
     return (
         <div className='button' onClick={ onClick }>{ text }</div>
