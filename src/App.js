7ea36a4fc02329ade9594e3f931dd1ab81af0524
Stefan Piazza <stefanpiazza@gmail.com>
2018-02-11 11:32:12 +0000

    UPDATE: change routes to exact to allow sub routes to error

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class App extends React.Component {
                     <div className='container'>
                         <Switch>
                             <Route exact path='/' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Home/Home') } />} />
-                            <Route path='/all' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/All/All') } />} />
-                            <Route path='/admin' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Admin/Admin') } />} />
+                            <Route exact path='/all' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/All/All') } />} />
+                            <Route exact path='/admin' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Admin/Admin') } />} />
                             <Route component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/NotFound/NotFound') } />} />
                         </Switch>
                     </div>
