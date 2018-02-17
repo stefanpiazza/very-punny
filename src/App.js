98014a93bf8e0b2f9a53237f5e62d3015f6d57b3
Stefan Piazza <stefanpiazza@gmail.com>
2018-02-17 16:41:29 +0000

    UPDATE: add puns to store on mount, render loading state otherwise

--- src/App.js
+++ src/App.js
@@ -6,31 +6,85 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import * as firebase from 'firebase';
+
 import Navigation from './components/Navigation/Navigation';
 import AsyncRoute from './components/AsyncRoute/AsyncRoute';
+import Loading from './components/Loading/Loading';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { addPun } from './actions';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isFetching: true
+        }
+    }
+
+    componentWillMount() {
+        const db = firebase.database();
+        const dbRef = db.ref();
+        const punsRef = dbRef.child('puns');
+        punsRef.once('value', snap => {
+            snap.val().map(pun => {
+                this.props.addPun(pun);
+            })
+        })
+        .then(() => {
+            this.setState({
+                isFetching: false
+            })
+        })
     }
 
     render() {
-        return (
-            <Router>
-                <div className='app'>
-                    <Navigation />
-                    <div className='container'>
-                        <Switch>
-                            <Route exact path='/' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Home/Home') } />} />
-                            <Route exact path='/all' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/All/All') } />} />
-                            <Route exact path='/admin' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Admin/Admin') } />} />
-                            <Route component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/NotFound/NotFound') } />} />
-                        </Switch>
+        if (this.state.isFetching) {
+            return (
+                <Router>
+                    <div className='app'>
+                        <Navigation />
+                        <div className='container'>
+                            <Loading text='Loading...'/>
+                        </div>
                     </div>
-                </div>
-            </Router>
-        )
+                </Router>
+            )
+        }
+
+        else {
+            return (
+                <Router>
+                    <div className='app'>
+                        <Navigation />
+                        <div className='container'>
+                            <Switch>
+                                <Route exact path='/' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Home/Home') } />} />
+                                <Route exact path='/all' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/All/All') } />} />
+                                <Route exact path='/admin' component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/Admin/Admin') } />} />
+                                <Route component={ (props) => <AsyncRoute props={ props } loading={ System.import('./containers/NotFound/NotFound') } />} />
+                            </Switch>
+                        </div>
+                    </div>
+                </Router>
+            )
+        }
     }
 }
 
-export default App
+function mapStatesToProps(state) {
+    return {
+        puns: state.puns
+    }
+}
+
+function matchDispatchToProps(dispatch) {
+    return bindActionCreators({
+        addPun: addPun
+    }, dispatch);
+}
+
+export default connect(mapStatesToProps, matchDispatchToProps)(App);
