ce44f2e7e852d8450db84790aa0526459c23e807
Stefan Piazza <stefanpiazza@gmail.com>
2018-03-17 13:10:36 +0000

    UPDATE: logged in layout added, firebase hooks added

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -9,10 +9,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 import Button from '../../components/Button/Button';
+import PunList from '../../components/PunList/PunList';
+import Pun from '../../components/Pun/Pun';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { userLogIn, userLogOut } from '../../actions';
+import { userLogIn, userLogOut, addPun, removePun } from '../../actions';
 
 class Admin extends React.Component {
     constructor(props) {
@@ -24,6 +27,8 @@ class Admin extends React.Component {
 
         this.handleLogIn = this.handleLogIn.bind(this);
         this.handleLogOut = this.handleLogOut.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
     }
 
     componentWillMount() {
@@ -62,7 +67,7 @@ class Admin extends React.Component {
                 document.getElementById('email').value = '';
                 document.getElementById('password').value = '';
 
-                this.props.userLogIn({ name: 'Stefan' })
+                this.props.userLogIn({ name: email })
             })
             .catch(() => {
                 document.getElementById('email').value = '';
@@ -80,22 +85,84 @@ class Admin extends React.Component {
             })
     }
 
+    renderLoggedIn() {
+        return (
+            <ul className='pun-list'>
+                <li className='pun-list__item'>
+                    <div className='pun'>
+                        <input type='text' className='joke input-joke' placeholder='Enter a joke' />
+                        <input type='text' className='punch-line input-punch-line' placeholder='Enter a punch line' />
+                    </div>
+                    <Button text='+' onClick={ () => this.handleAdd() } type='square' />    
+                </li>
+
+                {
+                    this.props.puns.map((pun, index) => {
+                        return (
+                            <li className='pun-list__item' key={ index } >
+                                <div className='pun'>
+                                    <p type='text' className='joke'>{ pun.joke }</p>
+                                    <p type='text' className='punch-line'>{ pun.punchLine }</p>
+                                </div>
+                                <Button text='-' onClick={ () => this.handleRemove(pun) } type='square' />    
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+
+    handleRemove(pun) {
+        this.props.removePun(pun);
+    }
+
+    handleAdd() {
+        const joke = document.getElementById('joke').value;
+        const punchLine = document.getElementById('punch-line').value;
+
+        if (!joke || !punchLine) return;
+
+        const pun = {
+            "joke": document.getElementById('joke').value,
+            "punchLine": document.getElementById('punch-line').value
+        }
+
+        const index = this.props.puns.length;
+
+        const db = firebase.database();
+        const dbRef = db.ref();
+        const punsRef = dbRef.child('puns');
+
+        punsRef.child(index).set(pun)
+        .then(() => {
+            this.props.addPun(pun);
+
+            document.getElementById('joke').value = '';
+            document.getElementById('punch-line').value = '';
+        })
+    }
+
     render() {
+        let adminLoggedIn = null;
         let adminLogin = null;
         let button = null;
 
         if (!this.props.user.isLoggedIn) {
+            adminLoggedIn = false;
             adminLogin = this.renderLogIn();
             button = <Button text='Log in' onClick={ this.handleLogIn } />
         }
 
         else {
+            adminLoggedIn = this.renderLoggedIn();
             adminLogin = null;
             button = <Button text='Log Out' onClick={ this.handleLogOut } />
         }
 
         return (
             <div className='admin'>
+                { adminLoggedIn }
                 { adminLogin }
                 { button }
             </div>
@@ -105,6 +172,7 @@ class Admin extends React.Component {
 
 function mapStatesToProps(state) {
     return {
+        puns: state.puns,
         user: state.user
     }
 }
@@ -112,7 +180,9 @@ function mapStatesToProps(state) {
 function matchDispatchToProps(dispatch) {
     return bindActionCreators({
         userLogIn: userLogIn,
-        userLogOut: userLogOut
+        userLogOut: userLogOut,
+        addPun: addPun,
+        removePun: removePun
     }, dispatch);
 }
 
